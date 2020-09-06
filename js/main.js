c23e75204373d7d57b345876ea56a03833a0a4dc
GSP-samurai <maddyson500@gmail.com>
2020-09-06 11:59:47 +0300

    feat: add swiper slider for section projects

--- js/main.js
+++ js/main.js
@@ -34,10 +34,29 @@ $(document).ready(function () {
         } else {
         upButton.removeClass('show');
         }
-      });
+    });
 
     upButton.on('click', function() {
         $('html, body').animate({scrollTop:0}, '400');
     });
+
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+
+    next.css('left', prev.width() + 10 + bullets.width() + 10)
+    bullets.css('left', prev.width() + 10 )
 
 });
