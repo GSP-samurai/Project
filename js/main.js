be8b790fe9fccc549d078c6e4eabe92d68dcebc2
GSP-samurai <maddyson500@gmail.com>
2020-11-04 15:54:02 +0300

    final version of this project

--- js/main.js
+++ js/main.js
@@ -102,20 +102,20 @@ $(document).ready(function () {
 
     $('[type=tel]').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
 
-    // var player;
+    var player;
 
-    // $('.video__play').on('click', function onYouTubeIframeAPIReady() {
-    //     player = new YT.Player('player', {
-    //         height: '465',
-    //         width: '100%',
-    //         videoId: 'MQ0DU4Wc79o',
-    //         events: {
-    //             'onReady': videoPlay,
-    //         }
-    //     });
-    // })
+    $('.video__play').on('click', function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '465',
+            width: '100%',
+            videoId: 'MQ0DU4Wc79o',
+            events: {
+                'onReady': videoPlay,
+            }
+        });
+    })
 
-    // function videoPlay(event) { 
-    //     event.target.playVideo();
-    //  }
+    function videoPlay(event) { 
+        event.target.playVideo();
+     }
 });
